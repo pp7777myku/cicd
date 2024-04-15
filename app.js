722706d0be55fe0b0a1092bce9eed6434378d907
pp7777myku <1176120476@qq.com>
2024-04-15 13:04:42 +0300

    v79

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
 // 设置静态文件夹
 app.use(express.static(path.join(__dirname, 'public')));
-
 // 主页路由
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 // 定义监听端口，使用环境变量或默认3000端口
 const PORT = process.env.PORT || 3000;
-
 // 将服务器监听放在一个函数中，以便测试时不启动服务器
 if (require.main === module) {
     app.listen(PORT, () => {
@@ -20,14 +16,6 @@ if (require.main === module) {
     });
 }
 
+// 导出app供测试使用
 // 导出app供测试使用 
 module.exports = app;
-
-const { JSDOM } = require('jsdom');
-const fs = require('fs');
-const path = require('path');
-
-const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf8');
-const { document } = (new JSDOM(html)).window;
-global.document = document;
-global.window = document.defaultView;
