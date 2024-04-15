f646e3e31271f55d097c4d1d6fe3213172bf6a1a
pp7777myku <1176120476@qq.com>
2024-04-15 13:12:40 +0300

    v80

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,26 @@
 const request = require('supertest');
-const app = require('../app'); 
+const app = require('../app');
 
+describe('GET static JavaScript file', () => {
+    it('responds with JavaScript file', async () => {
+        const response = await request(app)
+            .get('/script.js');
+
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('application/javascript');
+        expect(response.text).toContain('generateSensorData');
+    });
+});
 
 describe('GET /', () => {
-    it('should return the correct page title', async () => {
-      const response = await request(app).get('/');
-      expect(response.statusCode).toBe(200);
-      expect(response.text).toContain('Industrial Equipment Monitoring System');
+    it('responds with HTML containing sensors', async () => {
+        const response = await request(app)
+            .get('/');
+
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('text/html');
+        expect(response.text).toContain('Device 1 -');
+        expect(response.text).toContain('Device 10 -');
+        expect(response.text).toContain('generateSensorData');
     });
-  });
-  
+});
