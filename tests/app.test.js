44a464c8d2cfab729df0525c7fde116c7c54c913
pp7777myku <1176120476@qq.com>
2024-04-15 11:53:27 +0300

    v36

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/app'); // 假设你的主应用文件名为 app.js 并且位于 src 文件夹
+const app = require('../app'); 
 
 describe('GET /', () => {
   it('should return Welcome to CI/CD with GitHub Actions!', async () => {
