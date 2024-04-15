95c7c7b0a669aa99c2d2d8747fd37806082bb612
pp7777myku <1176120476@qq.com>
2024-04-15 11:59:32 +0300

    v40

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,8 @@
-const request = require('supertest');
-const app = require('../app'); 
-
 describe('GET /', () => {
-  it('should return Welcome to CI/CD with GitHub Actions!', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('Welcome to CI/CD with GitHub Actions!');
+    it('should return the correct page title', async () => {
+      const response = await request(app).get('/');
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toContain('Industrial Equipment Monitoring System');
+    });
   });
-});
+  
